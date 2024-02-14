c071f212c68be74c375548a58ec67982e73ed919
Ross F. Calimlim <ross.calimlim@gmail.com>
2024-02-14 15:36:06 -0500

    add lsp installs

--- install.sh
+++ install.sh
@@ -4,17 +4,21 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install binaries
-brew install \\
-  alacritty \\
-  eza \\
-  fish \\
-  fisher \\
-  fzf \\
-  helix \\
-  ripgrep \\
-  stow \\
-  xplr \\
-  zellij \\
+brew install \
+  alacritty \
+  eza \
+  fish \
+  fisher \
+  fzf \
+  helix \
+  node \
+  ripgrep \
+  sql-language-server \
+  stow \
+  vue-language-server \
+  yarn \
+  xplr \
+  zellij \
   visual-studio-code
 
 # clone dotfiles
@@ -22,3 +26,30 @@ mkdir -p $HOME/src/dotfiles
 git clone https://github.com/rcalimlim/dotfiles.git $HOME/src/dotfiles
 cd $HOME/src/dotfiles
 stow --restow -vv -t $HOME -d stow .
+
+# use modern yarn
+yarn set version berry
+
+# install helix lsps
+pnpm i -g \
+  dockerfile-language-server-nodejs \
+  graphql-language-service-cli \
+  svelte-language-server \
+  typescript \
+  typescript-language-server \
+  vls
+
+brew install \
+  bash-language-server \
+  gopls \
+  llvm \
+  lua-language-server \
+  marksman \
+  pnpm \
+  taplo \
+  tree-sitter \
+  vscode-langservers-extracted \
+  yaml-language-server \
+  zls
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # rust
