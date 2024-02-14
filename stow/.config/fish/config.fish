02d182aad870e34b606595c478a29152d509321c
Ross F. Calimlim <ross.calimlim@gmail.com>
2024-02-14 15:36:06 -0500

    add lsp installs

--- stow/.config/fish/config.fish
+++ stow/.config/fish/config.fish
@@ -61,3 +61,10 @@ if status is-interactive
     # eval (zellij setup --generate-auto-start fish | string collect)
 end
 
+
+# pnpm
+set -gx PNPM_HOME "/Users/ross/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
