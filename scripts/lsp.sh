4afcc64222deb2c9bd5f32ef338288d6d2c14c46
Ross F. Calimlim <ross.calimlim@gmail.com>
2024-03-22 19:02:20 -0400

    add makefile and fix repo

---
+++ scripts/lsp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pnpm i -g \
+  dockerfile-language-server-nodejs \
+  graphql-language-service-cli \
+  svelte-language-server \
+  typescript \
+  typescript-language-server \
+  vls
+
+brew install \
+  bash-language-server \
+  gopls \
+  llvm \
+  lua-language-server \
+  marksman \
+  pnpm \
+  taplo \
+  tree-sitter \
+  vscode-langservers-extracted \
+  yaml-language-server \
+  zls
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # rust
+
+# fish shell plugins managed by fisher
+fisher install pure-fish/pure
---
+++ scripts/pkg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# install binaries
+brew install --cask alacritty
+brew install \
+  corepack \
+  eza \
+  fish \
+  fisher \
+  fzf \
+  helix \
+  node \
+  ripgrep \
+  sql-language-server \
+  stow \
+  vue-language-server \
+  yarn \
+  xplr \
+  zellij \
+  visual-studio-code
