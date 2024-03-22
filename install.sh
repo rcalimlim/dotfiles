57a23381073ce6ad62cdcdfae17f06604afb9486
Ross Calimlim <rcalimlim@wayfair.com>
2024-03-22 13:55:03 -0400

    update

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ brew install \
 mkdir -p "$HOME"/src/dotfiles
 git clone https://github.com/rcalimlim/dotfiles.git "$HOME"/src/dotfiles
 cd "$HOME"/src/dotfiles || exit
+git submodule update --init --recursive
 stow --restow -vv -t "$HOME" -d stow .
 
 # use modern yarn
