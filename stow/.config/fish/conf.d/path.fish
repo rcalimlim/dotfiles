b47be89536f618e606b3ce1e8820e45ff92bfb4d
Ross F. Calimlim <ross.calimlim@gmail.com>
2024-03-27 23:02:48 -0400

    fix dirty fish_variables file with conf.d

---
+++ stow/.config/fish/conf.d/path.fish
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+# PATH variables
+#------------------------------------------------------------------------------
+fish_add_path /bin
+fish_add_path /sbin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/opt/llvm/bin
+fish_add_path /opt/pkg/bin
+fish_add_path /opt/pkg/sbin
+fish_add_path /opt/X11/bin
+fish_add_path /usr/bin
+fish_add_path /usr/sbin
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/sbin
+fish_add_path /usr/local/texlive/2023/bin/universal-darwin/ # latex
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/share/nvim/mason/bin
+fish_add_path $HOME/.local/bin
