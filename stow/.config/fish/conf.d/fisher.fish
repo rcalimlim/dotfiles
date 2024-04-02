87d8bb7ab46987a2f78657e6c67c450dd8d2c41d
Ross Calimlim <rcalimlim@wayfair.com>
2024-04-02 14:16:51 -0400

    attempt to fix fisher

--- stow/.config/fish/conf.d/fisher.fish
+++ stow/.config/fish/conf.d/fisher.fish
@@ -1,2 +1,13 @@
-# fisher install jorgebucaran/fisher
-# fisher install jorgebucaran/hydro
+# -a appends, so clear first
+set -e _fisher_plugins
+
+# fisher manages itself
+set -g _fisher_upgraded_to_4_4
+set -ga _fisher_plugins jorgebucaran/fisher
+
+set -g _fisher_jorgebucaran_2F_fisher_files ~/config/fish/functions/fisher.fish
+set -ga _fisher_jorgebucaran_2F_fisher_files ~/config/fish/completions/fisher.fish
+
+
+# hydro
+set -ga _fisher_plugins jorgebucaran/hydro
