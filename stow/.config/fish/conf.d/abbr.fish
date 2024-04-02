e1c111d16ab3c9fedbef6495f4c314fb5592f5f9
Ross Calimlim <rcalimlim@wayfair.com>
2024-04-02 14:19:55 -0400

    add abbrs

--- stow/.config/fish/conf.d/abbr.fish
+++ stow/.config/fish/conf.d/abbr.fish
@@ -1,6 +1,15 @@
 #------------------------------------------------------------------------------
 # Abbreviations (aliases that expand)
 #------------------------------------------------------------------------------
+# clear term
+abbr --add c clear
+
+# eza - modern ls alternative
+abbr --add l eza
+abbr --add ll eza --group --group-directories-first --all --icons --width 40
+abbr --add lla eza --group --header --group-directories-first --long --all --binary --icons
+
+# git
 abbr --add ga git add
 abbr --add gap git add -p
 abbr --add gs git status
@@ -13,9 +22,10 @@ abbr --add gr git reset head
 abbr --add glog git log --graph --decorate --pretty=oneline --abbrev-commit
 abbr --add ghd git reset --hard
 abbr --add gu git reset HEAD~1
-abbr --add c clear
+
+# xplr - file explorer
 abbr --add x xplr
+
+# zellij - terminal multiplexer
 abbr --add z zellij attach --create
-abbr --add l eza
-abbr --add ll eza --group --group-directories-first --all --icons --width 40
-abbr --add lla eza --group --header --group-directories-first --long --all --binary --icons
+abbr --add ztab zellij action new-tab -l default
